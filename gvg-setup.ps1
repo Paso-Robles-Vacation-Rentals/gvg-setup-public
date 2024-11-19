edfcd202b289e57f8f02b060ee9ad815bb97e697
Anthony <DeGariless@Gmail.com>
2024-11-19 11:27:11 -0800

    Update gvg-setup.ps1

--- gvg-setup.ps1
+++ gvg-setup.ps1
@@ -1,6 +1,6 @@
-echo "Starting gvg setup script"
+Write-Host "Starting gvg setup script"
 
-echo "enabling on-screen keyboard"
+Write-Host "enabling on-screen keyboard"
 # Define the registry path and property name
 $RegistryPath = "HKCU:\Software\Microsoft\TabletTip\1.7"
 # Ensure the registry key exists
@@ -9,6 +9,25 @@ if (-not (Test-Path $RegistryPath)) {
 }
 # Set the registry property
 Set-ItemProperty -Path $RegistryPath -Name "EnableDesktopModeAutoInvoke" -Value 1 -Type DWord
-echo "on-screen kayboard enabled"
+Write-Host "on-screen kayboard enabled"
 
-echo "gvg setup complete"
+
+Write-Host "changing GVG landing page"
+# Define the path to the file
+$filePath = "C:\apps\bnblauncher\routes\index.js"
+# Read the contents of the file
+$fileContent = Get-Content -Path $filePath
+# Replace the line containing var URL_PILOT
+$fileContent = $fileContent -replace 'var URL_PILOT = ".*";', 'var URL_PILOT = "https://www.pasoroblesvacationrentals.com/";'
+# Write the updated content back to the file
+Set-Content -Path $filePath -Value $fileContent
+Write-Host "GVG landing page set"
+
+
+Write-Host "==============================================="
+Write-Host "           GVG setup complete!               "
+Write-Host "   GVG will restart in 10 seconds...         "
+Write-Host "==============================================="
+Start-Sleep -Seconds 10
+# Reboot the system
+Restart-Computer -Force
