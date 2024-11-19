c2ca923edb05cea1db1fcddcdda0e7494422e87e
Anthony <DeGariless@Gmail.com>
2024-11-19 10:10:24 -0800

    Update gvg-setup.bat
    Delete self

--- gvg-setup.bat
+++ gvg-setup.bat
@@ -1,10 +1,12 @@
+# Enable on-screen keyboard
 # Define the registry path and property name
 $RegistryPath = "HKCU:\Software\Microsoft\TabletTip\1.7"
-
 # Ensure the registry key exists
 if (-not (Test-Path $RegistryPath)) {
     New-Item -Path $RegistryPath -Force | Out-Null
 }
-
 # Set the registry property
 Set-ItemProperty -Path $RegistryPath -Name "EnableDesktopModeAutoInvoke" -Value 1 -Type DWord
+
+# Delete self
+start "" cmd /c del "%~f0"
