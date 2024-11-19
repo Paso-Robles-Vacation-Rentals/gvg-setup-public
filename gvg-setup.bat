c6b4ca20a54666b2ffdb8e8b4a5740a4d78aacdb
Anthony <DeGariless@Gmail.com>
2024-11-19 10:07:25 -0800

    Create gvg-setup.bat
    init

---
+++ gvg-setup.bat
@@ -0,0 +1,10 @@
+# Define the registry path and property name
+$RegistryPath = "HKCU:\Software\Microsoft\TabletTip\1.7"
+
+# Ensure the registry key exists
+if (-not (Test-Path $RegistryPath)) {
+    New-Item -Path $RegistryPath -Force | Out-Null
+}
+
+# Set the registry property
+Set-ItemProperty -Path $RegistryPath -Name "EnableDesktopModeAutoInvoke" -Value 1 -Type DWord
